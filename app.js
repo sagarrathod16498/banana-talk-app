7c94e482d82442655cc0d2bcfbebebbe7b08c937
sagarrathod16498 <sagarrathod.16498@gmail.com>
2023-10-23 13:22:30 +0530

    import javascript file

---
+++ app.js
@@ -0,0 +1,8 @@
+//input
+var username = prompt("Give me username")
+
+//processing
+const welcomeMesgage=   console.log("script is working from different file");
+
+// output
+alert(welcomeMesgage);
