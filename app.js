c630e0be697ca93cf77d67e298712b1e7f68da15
sagarrathod16498 <sagarrathod.16498@gmail.com>
2023-10-26 17:38:51 +0530

    done

--- app.js
+++ app.js
@@ -2,11 +2,29 @@ const btnTaranslate = document.querySelector("#btn-translate");
 const txtInput = document.querySelector("#txt-input");
 const outputDiv = document.querySelector("#output");
 
-btnTaranslate.addEventListener("click", clickEventHandler);
+// const serverURL = "https:lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+const serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + text;
+}
+
+function errorHandler (error){
+    console.log("occure error" + error);
+    alert("something wrong with server! try again after some time")
+}
 
 function clickEventHandler() {
-    outputDiv.innerText= "jajajajajajajajajaj " + txtInput.value;
+    // outputDiv.innerText= "jajajajajajajajajaj " + txtInput.value;
+    var inputText = txtInput.value;
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => console.log(json.contents.translated))
+        .catch(errorHandler);
 };
 
 // outputDiv.innerText ="Sagar Rathod";
+btnTaranslate.addEventListener("click", clickEventHandler);
 
---
+++ server.js
@@ -0,0 +1,26 @@
+// const express = require ('express');
+// var cors = require('cors');
+
+// var app = express();
+// app.use(cors())
+
+// app.get("/", (req, res) => {
+//     res.send(" Go to translate")
+// })
+
+// app.get('/translate/yoda.json',(req, res) => {
+//     console.log (req.query.text)
+//     res.json({
+//         "success": {
+//             "total": 1
+//         },
+//         "contents": {
+//             "translated":"Testing, you are",
+//             "text": req.query.text || "text messing", 
+//             "translation": "yoda"}
+//     })
+// });
+
+// app.listen (3000, () => {
+//     console.log("server started");
+// })
